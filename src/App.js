2a7a7a73c82bf0415076571172712e8fc1fa02a9
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-24 09:35:13 +0200

    feat: add basic Navbar with navigation links to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import About from"./pages/About";
+import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

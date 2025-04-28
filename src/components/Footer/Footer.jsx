ae49ee9f0b13756bbb8b7a23626cb4a5f9e8cd28
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-28 15:58:26 +0200

    feat: add email link and improve footer links security

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,22 @@ import { FooterContainer, SocialLinks, SocialLink, CopyRight } from "./Footer.st
 
 function Footer() {
     return (
-      <FooterContainer>
-        <SocialLinks>
-          <SocialLink href="https://github.com/Dor-Ka" target="_blank">
-            <FaGithub />
-          </SocialLink>
-          <SocialLink href="https://www.linkedin.com/in/d-karpinska/" target="_blank">
-            <FaLinkedin />
-          </SocialLink>
-        </SocialLinks>
-        <CopyRight>&copy; 2025 DK Portfolio</CopyRight>
+        <FooterContainer>
+            <SocialLinks>
+                <SocialLink href="https://github.com/Dor-Ka" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </SocialLink>
+                <SocialLink href="https://www.linkedin.com/in/d-karpinska/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </SocialLink>
+                <SocialLink href="mailto:dorotakarpinska.dev@gmail.com">
+                    📧
+                </SocialLink>
+            </SocialLinks>
+
+            <CopyRight>&copy; 2025 DK Portfolio</CopyRight>
         </FooterContainer>
     );
-  }
-  
-  export default Footer;
+}
+
+export default Footer;

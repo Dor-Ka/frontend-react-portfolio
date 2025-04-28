b937523289d6f4bde4f46bc28d228f418d02518b
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-28 17:01:10 +0200

    feat: Implement global styles with box-sizing: border-box and layout structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GlobalStyle } from "./GlobalStyles";
+import { Wrapper, Content } from './styles';
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer/Footer"; 
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Wrapper>
+          <Navbar />
+          <Content>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Content>
+          <Footer />
+        </Wrapper>
+      </Router>
+    </>
   );
 }
 

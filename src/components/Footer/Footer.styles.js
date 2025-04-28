918f0b3dfe45718bd7e4fa3124a6b2a1765dc260
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-28 17:17:04 +0200

    Update styles for Footer and Navbar components

--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -1,25 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-  background: #333333;
-  color: #aaa;
-  text-align: center;
-  padding: 1rem 0;
-  font-size: 0.9rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 1rem;
+  background: #333;
+  color: white;
 `;
 
 export const SocialLinks = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 1.5rem;
-  font-size: 1.8rem;
+  gap: 2rem;
+  font-size: 2rem;
   margin-bottom: 0.5rem;
 `;
 
 export const SocialLink = styled.a`
   color: #e38327;
-  transition: color 0.3s, transform 0.3s;
   text-decoration: none;
+  transition: transform 0.3s, color 0.3s;
 
   &:hover {
     color: #fff;
@@ -28,6 +28,7 @@ export const SocialLink = styled.a`
 `;
 
 export const CopyRight = styled.p`
+  font-size: 0.9rem;
+  color: #aaa;
   margin: 0;
-  font-size: 0.8rem;
 `;

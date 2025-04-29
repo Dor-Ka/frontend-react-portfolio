62407521c36f41c70f69e4d38412035a4eeba008
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-29 17:00:51 +0200

    fix: switch to HashRouter for proper routing on localhost and GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Wrapper, Content } from './styles/styles';
 import { ThemeProvider } from 'styled-components';
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { lightTheme, darkTheme } from './styles/theme';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-import About from "./pages/About";
+import About from "./pages/About/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer/Footer";

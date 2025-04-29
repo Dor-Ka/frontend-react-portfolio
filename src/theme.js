5c2ed0116b66b1993ef6aba1fac2f4723bef8b92
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-04-29 12:33:52 +0200

    feat: create light and dark themes in theme.js

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+export const lightTheme = {
+    background: "#f4f4f4",
+    text: "#222",
+    primary: "#e38327",
+    secondary: "#333",
+    navBg: "#fff",
+    navText: "#e38327",
+    navHover: "#000",
+    footerBg: "#ddd",
+  };
+  
+  export const darkTheme = {
+    background: "#444",
+    text: "#fff",
+    primary: "#e38327",
+    secondary: "#222",
+    navBg: "#333",
+    navText: "#e38327",
+    navHover: "#fff",
+    footerBg: "#222",
+  };
+  

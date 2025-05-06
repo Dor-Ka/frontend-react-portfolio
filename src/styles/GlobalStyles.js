1c2cfb0c277640dc4dd02d24a28e094ac5e75dc0
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-06 19:07:53 +0200

    feat: integrate Fira Code font and refine global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,15 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 import 'normalize.css';
 
+
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
   }
 
+  html, body {
+    height: 100%;
+  }
+
   body {
-    font-family: Arial, sans-serif;
+    font-family: "Fira Code", monospace;
     background-color: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.text};
     transition: background-color 0.3s ease, color 0.3s ease;
